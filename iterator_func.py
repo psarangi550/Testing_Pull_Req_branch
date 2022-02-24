e69da31f6ab2ebabce8c79feeb8189f8baa01703
Pratik K Sarangi <81045908+psarangi550@users.noreply.github.com>
2022-02-24 09:05:49 +0530

    alternate_changes_commited

--- iterator_func.py
+++ iterator_func.py
@@ -1,6 +1,7 @@
 
 import itertools
 list1=[1,2,3,4]
-
+zip_list=list(zip(list1,itertools.repeat(2,times=4)))
+print(zip_list)
 list2=list(itertools.starmap(pow,list(zip(list1,itertools.repeat(2,times=4)))))
 print(list2)
